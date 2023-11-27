6a49aa082f87d693d4c8223345aa97da6de6b361
Osokina Maria <osokina.m@medindex.ru>
2023-11-27 21:45:18 +0300

    1.3. Webpack-dev-server. Переменные окружения (env)

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -3,12 +3,12 @@ import { BuildOptions } from "./types/config";
 import { buildPlugins } from "./buildPlugins";
 import { buildLoaders } from "./buildLoaders";
 import { buildResolvers } from "./buildResolvers";
-import path from "path";
+import { buildDevServer } from "./buildDevServer";
 
 export function buildWebpackConfig(
   options: BuildOptions
 ): webpack.Configuration {
-  const { paths, mode } = options;
+  const { paths, mode, isDev } = options;
 
   return {
     mode,
@@ -23,5 +23,7 @@ export function buildWebpackConfig(
       rules: buildLoaders(),
     },
     resolve: buildResolvers(),
+    devtool: isDev ? "inline-source-map" : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
   };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 import { someFn } from "./test";
 someFn(1);
+
+document.body.innerHTML = `<div>HELLO WORLD</div>`;
