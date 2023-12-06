805bd9351e02c48cde8953546a6fee261ee16175
Osokina Maria <osokina.m@medindex.ru>
2023-12-06 20:49:56 +0300

    2.12. Navbar. Шаблоны для разработки. Первый UI Kit компонент

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,17 +2,16 @@ import { Link, Route, Routes } from "react-router-dom";
 import "app/styles/index.scss";
 import { useTheme } from "app/providers/ThemeProvider/lib/useTheme";
 import { classNames } from "shared/lib/classNames/classNames";
-import { AppRouter } from "./providers/routes";
+import { AppRouter } from "app/providers/routes";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-
-      <Link to={"/"}>Главная</Link>
-      <Link to={"/about"}>О сайте</Link>
+      <Navbar />
       <AppRouter />
+      <button onClick={toggleTheme}>TOGGLE</button>
     </div>
   );
 };
