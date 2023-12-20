74f8fab8d68bc951bb31ad7d8957708a4aa3d9aa
Osokina Maria <osokina.m@medindex.ru>
2023-12-20 16:41:38 +0300

    2.21. Несуществующие маршруты. Лоадер для загрузки страниц

--- src/pages/MainPage/ui/MainPage.async.tsx
+++ src/pages/MainPage/ui/MainPage.async.tsx
@@ -1,12 +1,13 @@
 import { lazy } from "react";
 
 export const MainPageAsync = lazy(
-  async () =>
-    await new Promise((resolve) => {
-      // так в реальности делать не стоит!!!
-      // @ts-expect-error
-      setTimeout(() => {
-        resolve(import("./MainPage"));
-      }, 1500);
-    })
+    async () =>
+        await new Promise((resolve) => {
+            // так в реальности делать не стоит!!!
+      
+            setTimeout(() => {
+                // @ts-expect-error
+                resolve(import("./MainPage"));
+            }, 1500);
+        })
 );
