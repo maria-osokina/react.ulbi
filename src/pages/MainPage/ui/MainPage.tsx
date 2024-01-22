3fdace6838780a814419e0e0fb5c4c5b50361f6f
Osokina Maria <osokina.m@medindex.ru>
2024-01-22 15:58:20 +0300

    fix

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,15 +1,9 @@
 
-import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Input } from 'shared/ui/Input/Input'
 
 const MainPage = () => {
     const { t } = useTranslation()
 
-    const [value, setValue] = useState('')
-    const onChange = (val: string) => {
-        setValue(val);
-    }
     return (
         <div>
             {t('Главная страница')}
