f69070a74be23c470a2e1284972a7efc1c8685b9
Osokina Maria <osokina.m@medindex.ru>
2024-07-15 22:14:04 +0300

    6.46. React refresh plugin. build babel loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,28 +1,12 @@
 import type webpack from 'webpack'
 import { type BuildOptions } from './types/config'
 import { buildCssLoader } from './loaders/buildCssLoader'
+import { buildBabelLoader } from './loaders/buildBabelLoader'
 
-export function buildLoaders ({ isDev }: BuildOptions): webpack.RuleSetRule[] {
-    const babelLoader = {
-        test: /\.(js|jsx|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env'],
-                plugins: [
-                    [
-                        'i18next-extract',
-                        {
-                            locales: ['ru', 'en'],
-                            keyAsDefaultValue: true
-                        }
-                    ]
-                ]
-            }
-        }
-    }
-
+export function buildLoaders (options: BuildOptions): webpack.RuleSetRule[] {
+    const { isDev } = options;
+    
+    const babelLoader = buildBabelLoader(options)
     const cssLoader = buildCssLoader(isDev)
 
     const typescriptLoader = {
