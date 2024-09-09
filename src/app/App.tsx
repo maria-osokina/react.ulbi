23620a42047ef39c59b9e38aef01ac1d4b8e7578
Osokina Maria <osokina.m@medindex.ru>
2024-09-09 21:50:36 +0300

    7.47. Router v6 private protectеd routes. Защищенные маршруты

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,12 +4,13 @@ import { Navbar } from 'widgets/Navbar'
 import { Sidebar } from 'widgets/Sidebar'
 import { Suspense, useEffect } from 'react'
 import { useTheme } from './providers/ThemeProvider'
-import { useDispatch } from 'react-redux'
-import { userActions } from 'entities/User'
+import { useDispatch, useSelector } from 'react-redux'
+import { getUserInited, userActions } from 'entities/User'
 
 const App = () => {
     const { theme } = useTheme();
     const dispatch = useDispatch();
+    const inited = useSelector(getUserInited);
     
     useEffect(()=>{
         dispatch(userActions.initAuthData())
@@ -25,7 +26,7 @@ const App = () => {
                 <Navbar />               
                 <div className="content-page">
                     <Sidebar />
-                    <AppRouter />
+                    {inited && <AppRouter />}
                 </div>
             </Suspense>
         </div>
--- src/app/providers/routes/ui/AppRouter.tsx
+++ src/app/providers/routes/ui/AppRouter.tsx
@@ -1,37 +1,32 @@
-import { getUserAuthData } from 'entities/User'
-import { Suspense, memo, useMemo } from 'react'
-import { useSelector } from 'react-redux'
+import { Suspense, memo, useCallback } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { routeConfig } from 'shared/config/routeConfig/routeConfig'
 import { PageLoader } from 'widgets/PageLoader'
-
+import { RequireAuth } from './RequiredAuth';
+import { AppRoutesProps, routeConfig } from 'shared/config/routeConfig/routeConfig';
 
 const AppRouter = memo(() => {
-
-    const isAuth = useSelector(getUserAuthData);
-
-    const routes = useMemo(()=>{
-        return Object.values(routeConfig).filter(route => {
-            if (route.authOnly && !isAuth) {
-                return false;
-            }
-            return true;
-        })
-    },[isAuth]);
+    const renderWithWrapper = useCallback((route: AppRoutesProps) => {
+        const element = (
+            <Suspense fallback={<PageLoader />}>
+                <div className="page-wrapper">
+                    {route.element}
+                </div>
+            </Suspense>
+        );
+        return (
+            <Route
+                key={route.path}
+                path={route.path}
+                element={route.authOnly ? <RequireAuth>{element}</RequireAuth> : element}
+            />
+        );
+    }, []);
 
     return (
-        <Suspense fallback={<PageLoader />}>
-            <Routes>
-                {routes.map(({ element, path }) => (
-                    <Route
-                        key={path}
-                        element={<div className="page-wrapper">{element}</div>}
-                        path={path}
-                    />
-                ))}
-            </Routes>
-        </Suspense>
-    )
+        <Routes>
+            {Object.values(routeConfig).map(renderWithWrapper)}
+        </Routes>
+    );
 });
 
 export { AppRouter }
